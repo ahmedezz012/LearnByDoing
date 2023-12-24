cab52d3389559d8ad1aafad340a3def227bedd6d
Ahmed Ezz <ahmed_ezz2021995@yahoo.com>
2023-12-25 00:24:28 +0200

    Initial Commit

---
+++ Learn/LearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnApp.swift
+//  Learn
+//
+//  Created by Ahmed Ezz on 25/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct LearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
